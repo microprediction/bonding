366c4f2a619fcfb42483df4d1c10245d286cc963
peter cotton <peter.cotton@microprediction.com>
2025-01-23 17:58:32 -0500

    bonding curve decoupling

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="bonding",
-    version="0.0.1",
+    version="0.0.2",
     description="Bonding curve market makers",
     long_description=README,
     long_description_content_type="text/markdown",
